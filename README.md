a7e0559e0bbf08550c376c53cd60a19eb5930dc9
Prajit Patil <prajitpatil17@gamil.com>
2017-06-26 21:16:58 +0530

    Added feature to dump result at regular intervals, code refactoring, updated to only support python 3

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # Overview
 This is web crawler build using standard Python lib's.
 
+# Getting Started
+This code works with python 3.0 only
+```
+crawler = Crawler(outDir = 'tmp/crawler_result')
+crawler.start(
+    domain = 'https://en.wikipedia.org',                    # Explore pages within this domain
+    seedLink = 'https://en.wikipedia.org/wiki/Database',    # Start exploring from this page
+    dumpInterval = 10,                                      # Dump explored pages after every '10' exploration
+    maxExplore = 120)                                       # Explore maximum '120' pages
+# Above will create 13 files in 'tmp/crawler_result'
+# content of each file is a pickel representation of dump map, where key(link name) and value(link body) 
+```
+
 # Challenges
 ## Handling of links
 Links could be of various types varying from links within page, to link within domain to external links, etc. It very important that we handle links properly to make crawler more effective. We can categorise links in two major dimensions:
@@ -36,5 +49,5 @@ https://docs.google.com/viewer?a=v&pid=explorer&chrome=true&srcid=0B4t_NX-QeWDYZ
 ```
 
 # References
-* urllib2 : https://docs.python.org/2/howto/urllib2.html
-* Beautiful Soup : https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+* urllib: https://docs.python.org/3/library/urllib.request.html
+* Beautiful Soup: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
--- crawler.py
+++ crawler.py
@@ -1,34 +1,68 @@
-import urllib2
 from bs4 import BeautifulSoup
+import os
+import pickle
 import re
+import urllib
 
-def start_crawler(domain, seedLink, maxExplore = 100):
-    exploredLinkList = {};
-    candidateLinks = [seedLink];
-    for candidateLink in candidateLinks:
-        if candidateLink in exploredLinkList:
-            continue
-        if len(exploredLinkList) >= maxExplore:
-            break
-        exploredLinkList[candidateLink] = 1;
-        response = urllib2.urlopen(candidateLink);
-        soup = BeautifulSoup(response.read().decode("utf-8", "ignore"), 'html.parser')
-        for aTag in soup.find_all('a'):
-            link = aTag.get('href')
-            if link:
-                reInternalLink = re.compile(r"^/")      # Link that starts with '/'
-                match = reInternalLink.search(link)
-                if not match:
+
+class Crawler:
+    outDir = 'TMP'
+
+    def __init__(self, outDir):
+        self.outDir = outDir
+        if not os.path.exists(self.outDir):
+            os.makedirs(self.outDir)
+        print("Output directory: '%(dir)s'" %{'dir': outDir})
+
+    def dumpCache(self, fileName, content):
+        with open(os.path.join(self.outDir, fileName), 'wb') as fileHandle:
+            pickle.dump(content, fileHandle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    def isExternalLink(self, link):
+        reInternalLink = re.compile(r"^/")      # Link that starts with '/'
+        match = reInternalLink.search(link)
+        return (True if (not match) else False)
+        
+    def isDynamicLink(self, link):
+        reDynamicLink = re.compile(r"\?")       # Link that has '?'
+        match = reDynamicLink.search(link)
+        return (True if (match) else False)
+    
+    def start(self, domain, seedLink, dumpInterval = 10, maxExplore = 100):
+        exploredLinkList = {}
+        candidateLinks = [seedLink]
+        cacheContent = {}
+        fileIndex = 0
+        for candidateLink in candidateLinks:
+            if candidateLink in exploredLinkList:
+                continue
+            if len(exploredLinkList) >= maxExplore:
+                break
+            exploredLinkList[candidateLink] = 1;
+            response = urllib.request.urlopen(candidateLink);
+            responseBody = response.read().decode('utf-8');
+            soup = BeautifulSoup(responseBody, 'html.parser')
+            cacheContent[candidateLink] = responseBody
+            print ('.', end = '', flush = True)    # Will print '.' for every page it explore, visual effect to show progress
+            if len(cacheContent) >= dumpInterval:
+                self.dumpCache(str(fileIndex), cacheContent)
+                fileIndex += 1
+                cacheContent = {}
+            for aTag in soup.find_all('a'):
+                link = aTag.get('href')
+                if not link:                # skip if 'href' attribute is missing
                     continue
-                reDynamicLink = re.compile(r"\?")       # Link that has '?'
-                match = reDynamicLink.search(link)
-                if match:
+                if self.isExternalLink(link):
+                    continue
+                if self.isDynamicLink(link):
                     continue
                 link = domain + link 
                 candidateLinks.append(link)
-    for page in exploredLinkList.keys():
-        print page
+        self.dumpCache(str(fileIndex), cacheContent)
+        print("\n%(count)d pages explored" %{'count':len(exploredLinkList)})
 
-domain = 'https://en.wikipedia.org'
-seedLink = 'https://en.wikipedia.org/wiki/Database'
-start_crawler(domain, seedLink, maxExplore = 10);
+if __name__ == '__main__':
+    domain = 'https://en.wikipedia.org'
+    seedLink = 'https://en.wikipedia.org/wiki/Database'
+    crawler = Crawler('tmp')
+    crawler.start(domain, seedLink, dumpInterval = 3, maxExplore = 10)
